ad94df1c94f000699235bb6e3ba9b7597448969c
mohamed hassan <141786275+mohamedhassann27@users.noreply.github.com>
2025-09-20 00:13:05 +0300

    Update SummerSale.jsx

--- Final Project/src/components/home/SummerSale.jsx
+++ Final Project/src/components/home/SummerSale.jsx
@@ -3,7 +3,7 @@ function SummerSale() {
     return (
         <section className='py-10 my-10 flex relative h-110'>
             <div data-aos="zoom-out" className="w-full">
-                <img src="/public/imgs/Hero/banneroffer.png" alt="banner offer" className="w-full h-full mx-auto" />
+                <img src="imgs/Hero/banneroffer.png" alt="banner offer" className="w-full h-full mx-auto" />
             </div>
             <div data-aos="zoom-out" className="absolute left-[12%] top-[50%] translate-y-[-50%] max-md:left-[7%]">
                 <p className="text-5xl font-bold max-md:text-3xl">Summer Sale 50%</p>
@@ -15,3 +15,4 @@ function SummerSale() {
 }
 
 export default SummerSale
+
