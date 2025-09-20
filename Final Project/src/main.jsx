48bb46b4dfe2c651c7d615c84f5008b0ae2b293d
mohamed hassan <141786275+mohamedhassann27@users.noreply.github.com>
2025-09-20 12:02:46 +0300

    Update main.jsx

--- Final Project/src/main.jsx
+++ Final Project/src/main.jsx
@@ -8,6 +8,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
-    </HashRouter>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </StrictMode>,
 )
